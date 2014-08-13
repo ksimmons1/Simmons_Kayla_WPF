4c5a210b48640c3d8d4c3ec61f3b54187fd27fd4
ksimmons1 <kaylasimmons@kaylas-mbp.pk5001z>
2014-08-13 14:39:20 -0500

    PEMDAS Order of Operations
    In math you solve in order. Don’t use parenthesis if you don’t need
    them.

--- AssociatingJS/JS/script.js
+++ AssociatingJS/JS/script.js
@@ -1,4 +1,10 @@
-//Expressions - Modulo Operator
+//Expressions - PEMDAS - The Order of Operations
 
-var remainder = 211 % 2;
-console.log(remainder);
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3= 60;
+var quiz4 = 80;
+
+// the average is the sum of the items divided by the number of items
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+console.log(average);
