cb4d6d21fb6002e163b722062046474e1b1c5139
ksimmons1 <kaylasimmons@kaylas-mbp.pk5001z>
2014-08-14 15:01:57 -0500

    Personal Expression Complete
    How many tiles are needed to complete a room. Expression is completed
    and it works!!

--- Expressions_Personal/JS/script.js
+++ Expressions_Personal/JS/script.js
@@ -1,1 +1,16 @@
 //Personal Expression
+// How much tile do I need for my new dining room?
+
+var length = prompt ("How long is the room you are tiling in feet?", 30);
+// length of the room you are tiling
+
+var width = prompt ("How wide is the room you are tiling in feet?", 12);
+//width of the room you are tiling
+
+var tileSize = prompt ("What size tile are you wanting to use in sq. feet?", 1);
+// what size tile are you wanting to use?
+
+var howMuchTile = length * width / tileSize;
+// How much tile you need is the size of the room, which is length times width, divided by the tile size
+
+console.log( "You need " + howMuchTile + " tiles to complete your room. ");
