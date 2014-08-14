27333cdb3d49dc2d698c70101a926ba23adea2a4
ksimmons1 <kaylasimmons@kaylas-mbp.pk5001z>
2014-08-14 13:14:00 -0500

    Wrote in variables
    var weeklyUse with prompt
    var spills with prompt
    var caseSize with prompt

---
+++ Expressions_Industry/JS/script.js
@@ -0,0 +1,12 @@
+// Industrial Expression - How many cases of call bourbon will be needed for 1 year at the bar?
+
+//
+
+var weeklyUse = prompt ("How " + "many " + "drinks " + "of " + "call " + "bourbon " + "are " + "sold " + "weekly?", 550);
+// There are just over 60 shots per bottle of bourbon. Each drink and shot are one shot unless it is a double then it is two shots
+
+var spills = prompt ("How " + "many " + "spills " + "are " + "there " + "weekly?", 7);
+// Each spill or oops is recorded for inventory so we know how much less alcohol we have and aren't charging for the drink
+
+var caseSize= prompt ("How " + "many " + "bottles " + "of " + "bourbon " + "are " + "in " + "a " + "case?", 12);
+// Depending on where you order from there may be a different number of bottles per case
