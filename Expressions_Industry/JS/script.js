068e7d157aa7990a363c5c211803f70da89bd1e7
ksimmons1 <kaylasimmons@kaylas-mbp.pk5001z>
2014-08-14 13:24:07 -0500

    Start of equations
    Getting the first part of figuring the case amount needed for the year
    down and working

--- Expressions_Industry/JS/script.js
+++ Expressions_Industry/JS/script.js
@@ -1,6 +1,5 @@
 // Industrial Expression - How many cases of call bourbon will be needed for 1 year at the bar?
 
-//
 
 var weeklyUse = prompt ("How " + "many " + "drinks " + "of " + "call " + "bourbon " + "are " + "sold " + "weekly?", 550);
 // There are just over 60 shots per bottle of bourbon. Each drink and shot are one shot unless it is a double then it is two shots
@@ -10,3 +9,10 @@ var spills = prompt ("How " + "many " + "spills " + "are " + "there " + "weekly?
 
 var caseSize= prompt ("How " + "many " + "bottles " + "of " + "bourbon " + "are " + "in " + "a " + "case?", 12);
 // Depending on where you order from there may be a different number of bottles per case
+
+var numberOfBottles = (weeklyUse + spills)/ 60
+// finding the number of bottles used allows you to find how many cases are needed
+
+// Equation PEMDAS
+// weeklyUse + spills / 60 = how many bottles
+// case size / how many bottles * 52 weeks = how many cases
