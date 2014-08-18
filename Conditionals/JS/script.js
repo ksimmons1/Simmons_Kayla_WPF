36a397d679c1d8e016f9633e862efd1c65548551
ksimmons1 <kaylasimmons@kaylas-mbp.pk5001z>
2014-08-18 17:57:04 -0500

    Else If command
    if the first statement is false only then will it check the second
    statement to see if it is true. if 45” you can ride coaster with a
    parent

--- Conditionals/JS/script.js
+++ Conditionals/JS/script.js
@@ -1,12 +1,14 @@
-//Conditional Logic
+//Conditional Logic- else if
 
-var kidHeight = 49;
+var kidHeight = 47;
 var minHeight = 48;
-
+var wParentHeight = 45;
 
 //If the child is over 48" in height, print to the console "you can ride!"
 if(kidHeight > minHeight){
     console.log("You can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+    console.log("You can ride but only with a parent present!");
 }else{
     console.log("Sorry kid, you've got some growing to do first!");
 }
