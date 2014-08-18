19480629002c405fbe0a8740a0fcda370919cb72
ksimmons1 <kaylasimmons@kaylas-mbp.pk5001z>
2014-08-18 17:50:28 -0500

    If and else
    instead of writing another conditional you can put an else command
    after the } and tell the code to do a different action

--- Conditionals/JS/script.js
+++ Conditionals/JS/script.js
@@ -1,10 +1,13 @@
 //Conditional Logic
 
-var kidHeight = 47;
+var kidHeight = 49;
 var minHeight = 48;
-var sneakerLift = 2;
+
 
 //If the child is over 48" in height, print to the console "you can ride!"
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
     console.log("You can ride the coaster!");
+}else{
+    console.log("Sorry kid, you've got some growing to do first!");
 }
+
