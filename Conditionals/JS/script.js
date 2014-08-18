c6df75297b210d00c116edd1c5a8b215d9e2c0f5
ksimmons1 <kaylasimmons@kaylas-mbp.pk5001z>
2014-08-18 17:44:33 -0500

    Condition with Expression
    Adding height to the child from shoes. Could make him tall enough to
    ride coaster

--- Conditionals/JS/script.js
+++ Conditionals/JS/script.js
@@ -1,9 +1,10 @@
 //Conditional Logic
 
-var kidHeight = 50;
+var kidHeight = 47;
 var minHeight = 48;
+var sneakerLift = 2;
 
 //If the child is over 48" in height, print to the console "you can ride!"
-if(kidHeight > minHeight){
+if(kidHeight + sneakerLift > minHeight){
     console.log("You can ride the coaster!");
 }
