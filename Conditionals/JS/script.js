3c6a925230f97075f8439ac179ae2f7b9bcd13f8
ksimmons1 <kaylasimmons@kaylas-mbp.pk5001z>
2014-08-18 17:41:44 -0500

    Relational Expressions
    if kidHeight > minHeight

--- Conditionals/JS/script.js
+++ Conditionals/JS/script.js
@@ -1,8 +1,9 @@
 //Conditional Logic
 
-var oldEnough = false;
+var kidHeight = 50;
+var minHeight = 48;
 
-//If the child is old enough, print to the console "you can ride!"
-if(oldEnough){
+//If the child is over 48" in height, print to the console "you can ride!"
+if(kidHeight > minHeight){
     console.log("You can ride the coaster!");
 }
