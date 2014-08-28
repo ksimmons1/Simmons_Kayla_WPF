313729236052ccbce91c098f7b428963d76a2772
ksimmons1 <kaylasimmons@kaylas-mbp.pk5001z>
2014-08-27 20:47:30 -0500

    Do While loops
    do{
    }
    while( )
    action is ran before the condition so if condition is false the action
    will still be ran before it is checked.

--- Loops/JS/script.js
+++ Loops/JS/script.js
@@ -1,8 +1,8 @@
-// Loops - While
+// Loops - Do While
 
-var b = 50; // sets up the index
-
-while ( b > 0 ){ // checks the condition
-    console.log(b + " Kegs on the wall");
-    b--; // increments or decrements the index
+var c = 10;
+do{
+    console.log(c + " kegs on the wall");
+    c--;
 }
+while(c > 0);
