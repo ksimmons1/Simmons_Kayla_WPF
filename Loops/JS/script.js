90f377eead4a99e4d9544b60b4223ef48be49da0
ksimmons1 <kaylasimmons@kaylas-mbp.pk5001z>
2014-08-27 20:43:05 -0500

    While Loop
    Set up folder for loops
    While loop is like a condition but with while instead of if
    while (){
    }
    has index = variable
    while statement
    increment or decrement to end loop at certain number declared in the
    condition

---
+++ Loops/JS/script.js
@@ -0,0 +1,8 @@
+// Loops - While
+
+var b = 50; // sets up the index
+
+while ( b > 0 ){ // checks the condition
+    console.log(b + " Kegs on the wall");
+    b--; // increments or decrements the index
+}
