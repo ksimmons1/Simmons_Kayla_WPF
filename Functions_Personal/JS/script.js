301b3f7e77d96c9ea2c346c59ddfbbb0a80d3ce6
ksimmons1 <kaylasimmons@kaylas-mbp.pk5001z>
2014-08-29 12:12:54 -0500

    created personal function
    function needed to figure out how many gallons of water your fish tank
    will need to be filled. Can use it any time you want to fill your tank
    or someone else’s tank

--- Functions_Personal/JS/script.js
+++ Functions_Personal/JS/script.js
@@ -1,2 +1,14 @@
 //Kayla Simmons 8-29-2014 Functions Assignment
 
+
+
+function waterNeeded(gals){
+    var length = prompt("Enter length of fish tank in inches.");
+    var width = prompt("Enter width of fish tank in inches.");
+    var height = prompt("Enter height of fish tank in inches");
+    var totalSquareInches= length * width * height;
+    var gallonsOfWater = totalSquareInches / gals;
+    console.log(gallonsOfWater + " Gallons of water are needed to fill your fish tank.")
+}
+
+waterNeeded(316.42);
