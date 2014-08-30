63cae78eaafe05878f25578632ab4a8e5dac4a0b
ksimmons1 <kaylasimmons@kaylas-mbp.pk5001z>
2014-08-30 10:05:43 -0500

    Making a keno ticket
    code written and it was hard! math.random and math.round are used.
    produces a bullseye and your number. Also tells you how many games the
    numbers are good for! Just wish I could get the numbers to show up on
    one line!

--- Functions_Industry/JS/script.js
+++ Functions_Industry/JS/script.js
@@ -1,2 +1,24 @@
 //Kayla Simmons 8-29-14 Functions Assignment
 
+// Making a Keno ticket
+
+var spot = prompt("How many spots?");
+var times = prompt("How many times?");
+
+function keno(){
+    var startingNum = 1;
+    var endingNum = 80;
+    var pickingNumbers = Math.round(Math.random() * (endingNum - startingNum) + Number(startingNum));
+
+    return pickingNumbers
+
+}
+
+var ticket = keno();
+console.log(ticket + " Bullseye if purchased.");
+
+for(var a = 0; a < spot; a++){
+    console.log(keno() + " These numbers are good for " + times + " games. Good luck!")
+}
+
+
