23538adf544844ffaa279364388423e7cafbd9f7
ksimmons1 <kaylasimmons@kaylas-mbp.pk5001z>
2014-08-30 01:19:44 -0500

    Code for Wacky Function
    How much time needed to smoke meat

--- Functions_Wacky/JS/script.js
+++ Functions_Wacky/JS/script.js
@@ -1,2 +1,11 @@
 //Kayla Simmons 8-29-2014 Functions Assignment
 
+// How long to smoke your meat. (We are having brisket this weekend!!)
+
+var poundsOfMeat= prompt("How many pounds of meat are you planning to smoke?");
+var timeToSmoke = function (weight){
+    var time = weight * 1.5;
+    return time
+};
+
+var smokeTime = timeToSmoke(poundsOfMeat);
