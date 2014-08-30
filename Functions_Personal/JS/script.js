41d444fe6703a83ab67ee35602ff82e687f871a6
ksimmons1 <kaylasimmons@kaylas-mbp.pk5001z>
2014-08-30 10:57:12 -0500

    Validate entry on personal function
    Used a while loop to make sure that a number was entered not left blank
    or a word entered

--- Functions_Personal/JS/script.js
+++ Functions_Personal/JS/script.js
@@ -1,8 +1,21 @@
 //Kayla Simmons 8-29-2014 Functions Assignment
 
 var length = prompt("Enter length of fish tank in inches.");
+
+while(length === "" || isNaN(length)){
+    length = prompt("Please enter a correct length of fish tank in inches.");
+}
 var width = prompt("Enter width of fish tank in inches.");
+
+while(width === "" || isNaN(width)){
+    width = prompt("Please enter a correct width of fish tank in inches.");
+}
+
 var height = prompt("Enter height of fish tank in inches");
+
+while(height === "" || isNaN(height)){
+    height = prompt("Please enter a correct height of fish tank in inches");
+}
 
 function waterNeeded(long, wide, tall) {
     var gals = 316.42;
